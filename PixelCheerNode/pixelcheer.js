2b8efb2d71a4367f5510bca30ee8b1af9f6235fa
Adrian McEwen <adrianm@mcqn.com>
2014-12-12 14:14:40 +0000

    Initial version of NodeRED node to talk to the FACT facade

---
+++ PixelCheerNode/pixelcheer.js
@@ -0,0 +1,53 @@
+// Class to draw the displays for both browser and in node.js for the
+// LED facade at FACT Liverpool for PixelCheer
+// 
+// (c) Copyright 2014 MCQN Ltd
+//
+
+var CheerlightColours = {
+    "red": "rgb(255, 0, 0)",
+    "green": "rgb(0, 128, 0)",
+    "blue": "rgb(0, 0, 255)",
+    "cyan": "rgb(0, 255, 255)",
+    "white": "rgb(255, 255, 255)",
+    "warmwhite": "rgb(253, 245, 230)",
+    "oldlace": "rgb(253, 245, 230)",
+    "purple": "rgb(128, 0, 128)",
+    "magenta": "rgb(255, 0, 255)",
+    "yellow": "rgb(255, 255, 0)",
+    "pink": "rgb(255, 192, 203)",
+    "orange": "rgb(255, 165, 0)"
+};
+
+function Facade(canvas_element) {
+    this.canvas = canvas_element;
+    this.context = this.canvas.getContext('2d');
+    this.topLeft = 0;
+    this.topRight = 0;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.colour = "black";
+}
+Facade.prototype.init = function() {
+    // Start with a black screen
+    this.context.fillStyle = this.colour;
+    this.context.fillRect(this.topLeft, this.topRight, this.width, this.height);
+}
+Facade.prototype.update = function(image, colour) {
+    // Fill in the background colour
+console.log("image "+image);
+console.log("colour "+colour);
+    this.colour = colour;
+    this.context.fillStyle = CheerlightColours[this.colour];
+    this.context.fillRect(this.topLeft, this.topRight, this.width, this.height);
+    // And draw in the image
+    this.context.drawImage(image, 0, 0);
+}
+
+// Needed so we can import it into Node.js
+try {
+  exports.Facade = Facade;
+} catch (e) {
+  //alert("Error: "+e);
+}
+
---
+++ PixelCheerNode/pixelcheernode.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2014 MCQN Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var dgram = require('dgram');
+    var fs = require('fs');
+    var pc =  require('./pixelcheer');
+    var Canvas =  require('canvas');
+    var Image = Canvas.Image;
+    var image_names = [
+        "bauble",
+        "bell",
+        "franken_santa",
+        "jumper",
+        "nose",
+        "peaceful_santa",
+        "prints",
+        "pudding",
+        "silly_snowman",
+        "sleepy_santa",
+        "tree",
+        "xmasfc",
+        "yaksmas"
+    ];
+
+    function PixelCheer(n) {
+        RED.nodes.createNode(this,n);
+        this.server = n.server;
+        this.port = 11999;
+        this.rows = 50;
+        this.cols = 50;
+        // Load all the images we'll need
+        this.images = {}
+        for (var img in image_names) {
+            this.images[image_names[img]] = new Image;
+            this.images[image_names[img]].src = fs.readFileSync(__dirname+"/images/"+image_names[img]+".png");
+        }
+        // Create a canvas for us to draw on
+        this.canvas = new Canvas(this.cols, this.rows);
+        this.facade_buffer = new Buffer(this.rows*this.cols*3);
+        this.facade = new pc.Facade(this.canvas);
+        this.socket = dgram.createSocket("udp4");
+        this.currentImage = this.images["bauble"];
+        var node = this;
+
+        node.updateBuffer = function() {
+            // Dump the canvas out as a bitmap
+            var ctx = node.canvas.getContext("2d");
+            var id = ctx.getImageData(0, 0, node.cols, node.rows);
+            // ImageData will give us an array of RGBA, we only want RGB
+            var j = 0;
+            for (var i = 0; i < id.data.length; i++) {
+                if (i % 4 != 3) {
+                    // It's not the A component
+                    node.facade_buffer[j++] = id.data[i];
+                }
+                // else just skip the A component
+            }
+        }
+
+        // Refresh the display at a set rate
+        node._interval = setInterval( function() {
+            // Just send the data out, it will already be filled with the
+            // image data from the canvas
+            node.socket.send(node.facade_buffer, 0, node.facade_buffer.length, node.port, node.server);
+        }, 4500);
+
+        node.on("close", function() {
+            clearInterval(node._interval);
+        });
+
+        node.on("input", function(msg) {
+            if (msg != null) {
+                if (msg.topic && msg.payload) {
+                    // We've got our pre-requisites
+                    if (msg.topic == "cheerlights") {
+                        console.log("New colour "+msg.payload);
+                        node.currentColour = msg.payload;
+                        // Update the facade
+                        node.facade.update(node.currentImage, node.currentColour);
+                        node.updateBuffer();
+                    } else if (msg.topic == "pixelcheer") {
+                        console.log("New image "+msg.payload);
+                        // Get image
+                        node.currentImage = node.images[msg.payload];
+                        // Update the facade
+                        node.facade.update(node.currentImage, node.currentColour);
+                        node.updateBuffer();
+                    }
+                } else {
+                    node.error("Missing either a msg.block or a msg.payload");
+                }
+            }
+        });
+    }
+
+    RED.nodes.registerType("pixelcheer out",PixelCheer);
+}
